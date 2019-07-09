5ba4e360750f2a676c0d86e45a86999832fe582f
mamba2410 <callummcgregor54@gmail.com>
2019-07-09 21:38:50 +0100

    Changed i3 workspace 3 script and changed active bar colour in stacked windows

--- .Xscripts/workspace_3.sh
+++ .Xscripts/workspace_3.sh
@@ -3,11 +3,14 @@
 case $1 in
 	"stacked")
 			i3-msg "workspace 3:; append_layout $HOME/.Xscripts/w3-stacked-st.json";
-			$TERMINAL; $TERMINAL; $TERMINAL;
+			$TERMINAL &
+			$TERMINAL &
+			$TERMINAL &
 		;;
 	"single")
 			i3-msg "workspace 3:; append_layout $HOME/.Xscripts/w3-single-st.json";
-			$TERMINAL; $TERMINAL;
+			$TERMINAL &
+			$TERMINAL &
 		;;
 	*)
 		;;
