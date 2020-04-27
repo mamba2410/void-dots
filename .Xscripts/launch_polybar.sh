dd31f9209ad622b38f47506be2aac285a374b786
mamba2410 <callummcgregor54@gmail.com>
2020-04-27 12:12:28 +0100

    Changed compton to picom

--- .Xscripts/launch_polybar.sh
+++ .Xscripts/launch_polybar.sh
@@ -2,6 +2,7 @@
 
 # Terminate already running bar instances
 killall -q polybar
+#pkill polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
