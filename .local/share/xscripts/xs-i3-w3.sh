1a60ca13cbc23042808d551a9e174ba803aad328
mamba2410 <callummcgregor54@gmail.com>
2020-05-26 09:21:33 +0100

    Updated i3 w3 layout

--- .local/share/xscripts/xs-i3-w3.sh
+++ .local/share/xscripts/xs-i3-w3.sh
@@ -2,24 +2,23 @@
 
 case $1 in
 	"stacked")
-			i3-msg "workspace 3:; append_layout $HOME/.Xscripts/w3-stacked-st.json";
-			$TERMINAL &
-			$TERMINAL &
-			$TERMINAL &
+			i3-msg "workspace 3:; append_layout $XDG_DATA_HOME/xscripts/w3-stacked-st.json"
+			#$TERMINAL &
+			#$TERMINAL &
+			#$TERMINAL &
 		;;
 	"single")
-			i3-msg "workspace 3:; append_layout $HOME/.Xscripts/w3-single-st.json";
-			$TERMINAL &
-			$TERMINAL &
+			i3-msg "workspace 3:; append_layout $XDG_DATA_HOME/xscripts/w3-single-st.json";
+			#$TERMINAL &
+			#$TERMINAL &
 		;;
 	"latex")
-			i3-msg "workspace 3:; append_layout $HOME/.Xscripts/w3-latex-2term.json";
-			$TERMINAL &
-			$TERMINAL &
+			i3-msg "workspace 3:; append_layout $XDG_DATA_HOME/xscripts/w3-latex-2term.json";
+			#$TERMINAL &
+			#$TERMINAL &
 			#$TERMINAL &
 			mupdf *.pdf
 		;;
 	*)
 		;;
 esac
-
