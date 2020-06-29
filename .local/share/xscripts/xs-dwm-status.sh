d0e80ea4801e0940394c2fe94914f731fc3e9772
mamba2410 <callummcgregor54@gmail.com>
2020-06-29 12:32:38 +0100

    Changed i3 to dwm

---
+++ .local/share/xscripts/xs-dwm-status.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+while true; do
+
+	s_time="$(date +%H:%M)"
+	s_date="$(date +%Y/%m/%d)"
+	s_mem_total=$(grep "^MemTotal" /proc/meminfo | awk '{print $2}')
+	s_mem_free=$(grep "^MemFree" /proc/meminfo | awk '{print $2}')
+	#s_mem_m="$(( ($s_mem_total-$s_mem_free)/1024 ))M/$(( $s_mem_total/1024 ))M"
+	s_mem_p="$(( 100*($s_mem_total-$s_mem_free)/$s_mem_total ))%"
+	s_cpu_p=$(top -bn2 | grep "^\%Cpu" | awk ' FNR==1 {print $4}')%
+	s_vol_p=$(pactl list sinks | grep "^\WVolume" | awk '{print $5}') # $11 for right
+	
+	
+	status_string="C: $s_cpu_p  M: $s_mem_p  V: $s_vol_p  $s_time"
+	xsetroot -name "$status_string"
+	sleep 5
+	
+done
