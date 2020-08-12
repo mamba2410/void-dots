f4a77f3b49d3dae1de703cbbd85ea60d66b446c6
mamba2410 <callummcgregor54@gmail.com>
2020-08-12 23:28:24 +0100

    Update status script

--- .local/share/xscripts/xs-dwm-status.sh
+++ .local/share/xscripts/xs-dwm-status.sh
@@ -8,8 +8,9 @@ while true; do
 	s_mem_free=$(grep "^MemFree" /proc/meminfo | awk '{print $2}')
 	#s_mem_m="$(( ($s_mem_total-$s_mem_free)/1024 ))M/$(( $s_mem_total/1024 ))M"
 	s_mem_p="$(( 100*($s_mem_total-$s_mem_free)/$s_mem_total ))%"
-	s_cpu_p=$(top -bn2 | grep "^\%Cpu" | awk ' FNR==1 {print $4}')%
-	s_vol_p=$(pactl list sinks | grep "^\WVolume" | awk '{print $5}') # $11 for right
+	s_cpu_p="$(top -bn2 | grep "^\%Cpu" | awk ' FNR==1 {print $4}')%"
+	#s_vol_p=$(pactl list sinks | grep "^\WVolume" | awk '{print $5}') # $11 for right
+	s_vol_p=$(pactl list sinks | awk '/^\tVolume: /{print $5}' | sed -n 2p) # $11 for right, '2' is which sink to list
 	
 	
 	status_string="C: $s_cpu_p  M: $s_mem_p  V: $s_vol_p  $s_time"
