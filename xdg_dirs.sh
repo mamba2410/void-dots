0143fc84fd658fd329697ac94a5d0c066736560d
mamba2410 <callummcgregor54@gmail.com>
2024-02-07 17:40:24 +0000

    add xdg_dirs example

---
+++ xdg_dirs.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_RUNTIME_DIR="/tmp/run/$(id -u)"
+
+[ -d "$XDG_RUNTIME_DIR"] || mkdir "$XDG_RUNTIME_DIR"
+chown $(id -u) $XDG_RUNTIME_DIR
+
