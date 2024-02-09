cbac1a846cb88592a10671207ea93b44fcf4dcf3
unknown <callummcgregor54@gmail.com>
2024-02-09 12:54:55 +0000

    xdg_dirs.sh: fix runtime dir creation

--- xdg_dirs.sh
+++ xdg_dirs.sh
@@ -5,6 +5,6 @@ export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_RUNTIME_DIR="/tmp/run/$(id -u)"
 
-[ -d "$XDG_RUNTIME_DIR"] || mkdir "$XDG_RUNTIME_DIR"
+[ -d "$XDG_RUNTIME_DIR" ] || mkdir -p "$XDG_RUNTIME_DIR"
 chown $(id -u) $XDG_RUNTIME_DIR
 
